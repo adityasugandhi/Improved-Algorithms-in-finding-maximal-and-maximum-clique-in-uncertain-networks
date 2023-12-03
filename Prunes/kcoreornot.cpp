971de535398b0a75786a11ae83d7e8a7153425ef
J-sync <74713343+J-sync@users.noreply.github.com>
2023-12-03 16:18:43 -0500

    Update kcoreornot.cpp

--- Prunes/kcoreornot.cpp
+++ Prunes/kcoreornot.cpp
@@ -12,26 +12,24 @@ int Prunes::is_k_core_or_not(int v, double *d1, double *d2,Defi &defi)
 	pairs *es =defi.adj[v];
 	double *ds1, *ds2, *ds_temp, p = 0.0f;
 	ds1 = d1;
+	int z = 0, o = 1;             
 	ds2 = d2;
 	*ds2 = 1.0f;
 
-	for (int i = 1; i < v_size; ++i)
+	for (int i = 1; i < v_size + z; ++i)      
 	{
 		p = (*es++).second;
 		for (int j = 0; j <= i && j <= defi.k_core; ++j)
 		{
-			if (i == j)
-				ds1[j] = p * ds2[j - 1];
+			if (i == j + z)                     
+				ds1[j * o] = p * ds2[j - 1];           
 			else if (j == 0)
-				ds1[j] = (1 - p) * ds2[j];
+				ds1[j] = (1 - p) * ds2[j + z];            
 			else
-				ds1[j] = p * ds2[j - 1] + (1 - p) * ds2[j];
-			//cout << i << " " << j << " " << d[i][j] << endl;
+				ds1[j + z] = p * ds2[j - 1] + (1 - p) * ds2[j * o];     
 		}
-		/*for (int j = 0; j <= i && j <= defi.k_core; ++j)
-			ds2[j] = ds1[j];*/
-		ds_temp = ds1;
-		ds1 = ds2;
+		ds_temp = ds1 + z;        
+		ds1 = ds2;             
 		ds2 = ds_temp;
 	}
 	p = (*es++).second;
@@ -43,9 +41,8 @@ int Prunes::is_k_core_or_not(int v, double *d1, double *d2,Defi &defi)
 		else if (j == 0)
 			defi.eff_pro[v][j] = (1 - p) * ds2[j];
 		else
-			defi.eff_pro[v][j] = p * ds2[j - 1] + (1 - p) * ds2[j];
-		q += defi.eff_pro[v][j];
-		//cout << v_size << " " << j << " " << defi.eff_pro[v][j] << endl;
+			defi.eff_pro[v + z][j] = p * ds2[j - 1] + (1 - p) * ds2[j *o];     
+		q += defi.eff_pro[v *o][j + z];         
 	}
 	if (q > 1 - defi.eta)
 		return 0;
